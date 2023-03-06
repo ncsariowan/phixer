249364de02c38663679706ed3911d2593c4aae71
Gabecs347 <126730073+Gabecs347@users.noreply.github.com>
2023-03-06 13:44:46 -0800

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,13 @@
 import styles from './styles/home.module.css';
+import { Link } from '@solidjs/router';
 
 const Home = () => (
-  <div class={ styles.home }>
-    Home
+  <div class={styles.home}>
+    <h1 class={styles.title}>Welcome to our app</h1>
+    <p class={styles.subtitle}>Click below to get started</p>
+    <Link href="/app">
+      <button class={styles.button}>Start App</button>
+    </Link>
   </div>
 );
 
