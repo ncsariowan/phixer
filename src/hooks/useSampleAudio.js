6cc7216471597b3e91a22db9d0631497487b4419
Nathan Sariowan <ncsariowan@gmail.com>
2023-03-14 11:47:09 -0700

    implement stem playing; todo: mapping

--- src/hooks/useSampleAudio.js
+++ src/hooks/useSampleAudio.js
@@ -1,15 +1,36 @@
 const ctx = new AudioContext();
 
-const buffer = new AudioBufferSourceNode(ctx);
-const gain = new GainNode(ctx);
-const pan = new StereoPannerNode(ctx);
+class Stem {
+    constructor(url) {
+        this.loadBuffer(url);
 
-const res = await fetch('src/assets/sample.mp3');
-const arrayBuffer = await res.arrayBuffer();
-buffer.buffer = await ctx.decodeAudioData(arrayBuffer);
+        this.gainNode = new GainNode(ctx);
+        this.panNode = new StereoPannerNode(ctx);
+        this.gainNode.connect(this.panNode);
+        this.panNode.connect(ctx.destination);
+    }
 
-buffer.connect(gain);
-gain.connect(pan);
-pan.connect(ctx.destination);
+    async loadBuffer(url) {
+        var res = await fetch(url);
+        const arrayBuffer = await res.arrayBuffer();
+        this.buffer = await ctx.decodeAudioData(arrayBuffer);
+    }
 
-export default () => ({ buffer, gain, pan });
+    play(time) {
+        const sourceNode = ctx.createBufferSource();
+        sourceNode.connect(this.gainNode);
+        console.log(this.buffer);
+        sourceNode.buffer = this.buffer;
+
+        sourceNode.start(time);
+    }
+}
+
+const stemPaths = ["Guitar.wav", "Marie.wav", "Joey.wav", "Bass.wav", "Drums.wav"]
+var stems = [];
+
+for (var s of stemPaths) {
+    stems.push(new Stem("src/assets/stems/" + s));
+}
+
+export default () => ({ stems });
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -2,16 +2,38 @@ import Video from '../components/Video';
 import useSampleAudio from '../hooks/useSampleAudio';
 import useVideo from '../hooks/useVideo';
 import styles from '../styles/app.module.css';
+import { start } from '../util/video';
 
 const App = () => {
+  const ctx = new AudioContext();
+
   const { setStreaming } = useVideo();
-  const { buffer, gain, pan } = useSampleAudio();
+  const { stems } = useSampleAudio();
+
+  var streaming = false;
+  var playing = false;
 
   return (
-    <div class={ styles.app }>
+    <div class={styles.app}>
       <Video />
-      <button onClick={ () => setStreaming(true) }>Start</button>
-      <button onClick={ () => buffer.start() }>Play</button>
+      <button onClick={() => {
+        setStreaming(true);
+        // streaming = !streaming;
+        // setStreaming(streaming);
+
+        // playing = false;
+        // buffer.stop();
+      }
+      }>Start</button>
+      <button onClick={() => {
+
+        const startTime = ctx.currentTime + 0.5;
+        
+        for (const s of stems) {
+          s.play(startTime);
+        }
+
+      }}>Play</button>
     </div>
   );
 };
